fa57fe7d34a35cc6ba0d85f7ba80badf1adfb919
Dan Bikle <bikle@bikle.com>
2016-09-05 15:43:00 -0700

    hello

--- genf.py
+++ genf.py
@@ -14,3 +14,16 @@
 # I should have at least two SLOPE values and they should be between 2 and 32.
 
 # If you have questions, e-me: bikle101@gmail.com
+
+import numpy  as np
+import pandas as pd
+import pdb
+
+# I should check cmd line arg
+import sys
+pdb.set_trace()
+if (len(sys.argv) < 3):
+  print('Demo:')
+  print("~/anaconda3/bin/python genf.py TRAINSIZE=30 SLOPES='[3,5,9,17]'")
+  print('...')
+  sys.exit()
