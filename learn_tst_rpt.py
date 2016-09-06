ae893dfa3a2d3e063145338a6d9f9375ffea4198
Dan Bikle <bikle@bikle.com>
2016-09-05 17:04:06 -0700

    hello

---
+++ learn_tst_rpt.py
@@ -0,0 +1,7 @@
+# learn_tst_rpt.py
+
+# This script should learn from observations in /tmp/mlapp11/feat.csv
+
+# Then it should test its learned models on observations later than the training observations.
+
+# Next it should report effectiveness of the models.
