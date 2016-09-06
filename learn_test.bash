b9f548bfb35730bc137d9d93e557e3eb1af69cad
Dan Bikle <bikle@bikle.com>
2016-09-05 17:16:33 -0700

    hello

--- learn_test.bash
+++ learn_test.bash
@@ -3,7 +3,7 @@
 # learn_test.bash
 
 # Demo:
-# ./learn_test.bash TRAINSIZE=30 SLOPES='[3,5,9,17]'
+# ./learn_test.bash TRAINSIZE=30 TESTYEAR=2015 SLOPES='[3,5,9,17]'
 
 # This script should build two types of ML models:
 # Linear   Regression
@@ -11,7 +11,8 @@
 # from GSPC prices from Yahoo.
 # Then it should show a comparison of predictive effectiveness from the models.
 
-# TRAINSIZE should specify years of training data to learn from.
+# TRAINSIZE should specify years  of training data to learn from.
+# TESTYEAR  should specify a year of observations which I want to predict.
 # SLOPES should specify moving avg durations, in days, which I compute slopes from.
 # I should have at least two SLOPE values and they should be between 2 and 32.
 
@@ -31,20 +32,20 @@ sort /tmp/mlapp11/gspc.csv|awk -F, '{print $1"," $5}'|grep -v Date >> /tmp/mlapp
 
 # I should compute features from the prices:
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
   echo
   echo "You typed something wrong."
   echo "Try something like this:"
   echo "Demo:"
-  echo "./learn_test.bash TRAINSIZE=30 SLOPES='[3,5,9,17]'"
+  echo "./learn_test.bash TRAINSIZE=30 TESTYEAR=2015 SLOPES='[3,5,9,17]'"
   exit 1
 fi
 
-${HOME}/anaconda3/bin/python genf.py $2
+${HOME}/anaconda3/bin/python genf.py $3
 
 # I should learn, test, and report:
-${HOME}/anaconda3/bin/python learn_tst_rpt.py
+${HOME}/anaconda3/bin/python learn_tst_rpt.py $1 $2
 
 exit
 
--- learn_tst_rpt.py
+++ learn_tst_rpt.py
@@ -5,3 +5,9 @@
 # Then it should test its learned models on observations later than the training observations.
 
 # Next it should report effectiveness of the models.
+
+# Demo:
+# ~/anaconda3/bin/python genf.py TRAINSIZE=30 TESTYEAR=2015
+
+# Above demo will train from years 1984 through 2014 and predict each day of 2015
+
