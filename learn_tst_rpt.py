83173b81d643683105f7b1f3e9b66e8ac48f5c51
Dan Bikle <bikle@bikle.com>
2016-09-05 17:22:33 -0700

    hello

--- learn_tst_rpt.py
+++ learn_tst_rpt.py
@@ -11,3 +11,21 @@
 
 # Above demo will train from years 1984 through 2014 and predict each day of 2015
 
+import numpy  as np
+import pandas as pd
+import pdb
+
+# I should check cmd line args
+import sys
+if (len(sys.argv) != 3):
+  pdb.set_trace()
+  print('You typed something wrong:')
+  print('Demo:')
+  print("~/anaconda3/bin/python genf.py TRAINSIZE=30 TESTYEAR=2015")
+  sys.exit()
+pdb.set_trace()
+
+# I should get cmd line args:
+trainsize_i = int(sys.argv[1].split('=')[1])
+testyear_s  = sys.argv[2].split('=')[1]
+'bye'
