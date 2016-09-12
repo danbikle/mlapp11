2dd486b88709e326b4a82f5bdff331ec6cc19124
Dan Bikle <bikle@bikle.com>
2016-09-11 17:48:54 -0700

    more

--- whatif.bash
+++ whatif.bash
@@ -4,6 +4,30 @@
 
 # This script helps me answer: 'Whatif price is $x?'
 
-echo under construction
+# Demo:
+# ./whatif.bash TRAINSIZE=25 SLOPES='[2,3,5,9,17]' 2123.40
+
+if [ $# -ne 3 ]
+then
+  echo
+  echo "You typed something wrong."
+  echo "Try something like this:"
+  echo "Demo:"
+  echo "./whatif.bash TRAINSIZE=25 SLOPES='[2,3,5,9,17]' 2123.40"
+  exit 1
+fi
+
+# I should create a folder to hold temporary data:
+mkdir -p /tmp/mlapp11/
+
+# I should get prices from Yahoo:
+/usr/bin/curl http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC > /tmp/mlapp11/gspc.csv
+
+# I should extract two columns and also sort:
+echo cdate,cp                                                       > /tmp/mlapp11/gspc2.csv
+sort /tmp/mlapp11/gspc.csv|awk -F, '{print $1"," $5}'|grep -v Date >> /tmp/mlapp11/gspc2.csv
+# I should add the whatif price:
+tail -1 /tmp/mlapp11/gspc2.csv >> /tmp/mlapp11/gspc2.csv
+sed -i "$ s/,.*/,$3/" /tmp/mlapp11/gspc2.csv
 
 exit
